13ec260e1a3b358718036b7b14b25d5965fb1c57
Subhajyoti <subhajyotidey2910@gmail.com>
2026-03-12 19:00:31 +0530

    Repo initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{ 
+      minHeight: "100vh",
+      fontSize: "20px",
+      margin: "10px"
+    }}>
+      Hello World
+    </main>
+  );
+}
